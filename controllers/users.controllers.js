a8afaf57aae89fa3dd998073f9af11650ee45ec8
marcosAlvarezCalabria <marcosalvarezcalabria@gmail.com>
2024-05-03 15:40:48 +0100

    feat: add useEfect register/page

--- controllers/users.controllers.js
+++ controllers/users.controllers.js
@@ -8,11 +8,12 @@ module.exports.create = (req, res, next) => {
     const userData = req.body
     User.create(userData)
         .then((user) => {
+            console.log(user)
             res.json(user)
         })
         .catch((error) => {
             if( error instanceof mongoose.Error.ValidationError) {
-                res.status(401).json(error.error)
+                res.status(400).json(error.error)
             } else {
                 next(error)
             }
