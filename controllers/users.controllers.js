72e90f53230bc0ef054acc5c538dba178717598b
marcosAlvarezCalabria <marcosalvarezcalabria@gmail.com>
2024-05-05 16:39:25 +0100

    chore:form login fixed(invalid credentials

--- controllers/users.controllers.js
+++ controllers/users.controllers.js
@@ -62,7 +62,7 @@ module.exports.profile = (req, res, next) => {
 };
 
 module.exports.update = (req, res, next) => {
-    User.findByIdAndUpdate(req.user.id, {body:req.user.body},{ runValidators: true })
+    User.findByIdAndUpdate(req.user.id, req.body,{ runValidators: true, new: true })
         .then((user) => {
             if (user) {
                 res.json(user)
